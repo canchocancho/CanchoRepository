c9087b73c9fbfc714a11eaeb1f86697798964585
SCITMASTER <SCITMASTER@LAPTOP-S71HL543>
2018-03-30 15:23:03 +0900

    controller

--- CanchoProject/src/main/java/lets/eat/cancho/HomeController.java
+++ CanchoProject/src/main/java/lets/eat/cancho/HomeController.java
@@ -15,10 +15,17 @@ public class HomeController {
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
+	public String home(Model model) {
 		logger.info("HOME");
 		
 		return "home";
+	}
+	
+	@RequestMapping(value="goPost", method=RequestMethod.GET)
+	public String goPost(){
+		logger.info("POST");
+		
+		return "post";
 	}
 	
 }
